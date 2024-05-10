95941986969d46984d912277b01e628096950b97
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-05-10 09:18:11 +0800

    pos

--- src/app/pos/pos.component.ts
+++ src/app/pos/pos.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { AppService } from '../app.service';
 import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-pos',
   templateUrl: './pos.component.html',
-  styleUrl: './pos.component.scss'
+  styleUrls: ['./pos.component.scss']
 })
 export class PosComponent {
 
   products: any[] = [];
+  selectedProducts: any[] = [];
 
   constructor(public productService: ProductService) { }
 
@@ -17,10 +17,23 @@ export class PosComponent {
     // Subscribe to the products$ observable to get the list of products
     this.productService.products$.subscribe((products: any[]) => {
       if (products && products.length > 0) {
-        this.products = products;
-        console.log('Products received in LandingComponent:', this.products);
+        this.products = products.map(product => ({ ...product, counter: 0 }));
       }
     });
   }
+
+  addToRightDiv(product: any) {
+    // Check if the product already exists in selectedProducts
+    const existingProductIndex = this.selectedProducts.findIndex(selectedProduct => selectedProduct.product === product.product);
+
+    if (existingProductIndex === -1) {
+        // If the product doesn't exist, add it to selectedProducts
+        product.counter++;
+        this.selectedProducts.push(product);
+    } else {
+        // If the product already exists, increment its counter
+        this.selectedProducts[existingProductIndex].counter++;
+    }
+}
 
 }
