6dc192e363b413f4db0b4dfad341b3060679f7d4
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-05-13 10:56:30 +0800

    deploy

--- docs/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-3R77YJ6R.mjs";import"./chunk-NGMPCEP2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-R36JDG3K.mjs";import"./chunk-NGMPCEP2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
