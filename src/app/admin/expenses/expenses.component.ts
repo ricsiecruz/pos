aa022680b11e24a044c9d48cb8a0b02a28c49934
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-06-30 01:01:06 +0800

    expenses

--- src/app/admin/expenses/expenses.component.ts
+++ src/app/admin/expenses/expenses.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { AppService } from '../../app.service';
 import { ModalService } from '../../modal.service';
 import { ExpensesService } from '../../services/expenses.service';
@@ -9,10 +9,11 @@ import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-expenses',
   templateUrl: './expenses.component.html',
-  styleUrl: './expenses.component.scss'
+  styleUrls: ['./expenses.component.scss']
 })
 export class ExpensesComponent {
   API_URL = environment.apiUrl;
+  @ViewChild('sales') sales?: TemplateRef<any>;
   expenses: any[] = [];
   expense: string = '';
   amount?: number | null;
@@ -20,6 +21,8 @@ export class ExpensesComponent {
   totalSum: number = 0;
   newExpenses: any = { expense: '', month: '', date: '', amount: '', channel: '' };
   selectedFile: File | null = null;
+  imagePreviewUrl: string | null = null;  // Variable for image preview
+  details: any;
 
   constructor(
     private http: HttpClient,
@@ -34,56 +37,60 @@ export class ExpensesComponent {
     this.expensesService.expenses$.subscribe((products: any[]) => {
       if (products && products.length > 0) {
         this.expenses = products;
-        console.log('a', products)
         this.calculateTotalSum(products);
-        console.log('sum', this.calculateTotalSum(products))
       }
     });
 
     this.webSocketService.receive().subscribe((message: any) => {
       if (message.action === 'addExpenses') {
-        console.log('b', message.product)
         this.expenses.push(message.product);
       }
     });
   }
+
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
-}
-addProduct() {
-  const currentDate = new Date();
-  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const currentMonth = monthNames[currentDate.getMonth()];
-  const currentYear = currentDate.getFullYear();
-  this.newExpenses.month = `${currentMonth} ${currentYear}`;
-  this.newExpenses.date = new Date().toISOString();
-  console.log('addProduct', this.newExpenses)
-  if (this.selectedFile) {
-    console.log('here')
+    if (this.selectedFile) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imagePreviewUrl = e.target.result;
+      };
+      reader.readAsDataURL(this.selectedFile);
+    }
+  }
+
+  addProduct() {
+    const currentDate = new Date();
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const currentMonth = monthNames[currentDate.getMonth()];
+    const currentYear = currentDate.getFullYear();
+    this.newExpenses.month = `${currentMonth} ${currentYear}`;
+    this.newExpenses.date = new Date().toISOString();
+
+    if (this.selectedFile) {
       const formData = new FormData();
       formData.append('image', this.selectedFile, this.selectedFile.name);
       this.http.post<{ imagePath: string }>(this.API_URL + 'expenses/upload', formData).subscribe(
-          (response) => {
-              this.newExpenses.image_path = response.imagePath;
-              this.sendExpenseData();
-          },
-          (error) => {
-              console.error('Image upload failed:', error);
-          }
+        (response) => {
+          this.newExpenses.image_path = response.imagePath;
+          this.sendExpenseData();
+        },
+        (error) => {
+          console.error('Image upload failed:', error);
+        }
       );
-  } else {
-    console.log('???')
+    } else {
       this.sendExpenseData();
+    }
   }
-}
 
-sendExpenseData() {
-  console.log('sendExpenseData', this.newExpenses)
-  this.expensesService.addExpenses(this.newExpenses);
-  this.modalService.closeModal();
-  this.newExpenses = { expense: '', month: '', date: '', amount: '', channel: '' };
-}
-  
+  sendExpenseData() {
+    this.expensesService.addExpenses(this.newExpenses);
+    this.modalService.closeModal();
+    this.newExpenses = { expense: '', month: '', date: '', amount: '', channel: '' };
+    this.imagePreviewUrl = null; // Clear the preview after submitting
+  }
+
   private calculateTotalSum(expenses: any[]): number {
     return expenses.reduce((acc, curr) => acc + parseFloat(curr.amount), 0);
   }
@@ -91,7 +98,13 @@ sendExpenseData() {
   loadInventory() {
     this.appService.getExpenses().subscribe((res: any) => {
       this.expenses = res;
-    })
+    });
+  }
+
+  view(product: any) {
+    this.details = product;
+    console.log('details', this.details)
+    this.modalService.openModal(this.sales);
   }
 
   openAddInventoryModal(modalContent: any) {
