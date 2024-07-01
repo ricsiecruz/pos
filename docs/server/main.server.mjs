05e872e74dd2a405465c6556c95bdc70018ab619
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-07-01 21:55:23 +0800

    deploy

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-45WUWYNN.mjs";import"./chunk-IZ7FIPOT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-FERGM22J.mjs";import"./chunk-IZ7FIPOT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
