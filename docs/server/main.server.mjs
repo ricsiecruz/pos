ac25cca6288a7b7e5ad70f292d4a5772cb7481d2
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-07-01 18:03:19 +0800

    deploy

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-I3OIJBVX.mjs";import"./chunk-GQJ2OPLS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-7V4MDFXJ.mjs";import"./chunk-IZ7FIPOT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/websocket-service.ts
+++ src/app/websocket-service.ts
@@ -1,42 +1,63 @@
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, Inject, Injectable } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { environment } from '../environments/environment';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, EMPTY, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
-  private socket$: WebSocketSubject<any>;
+  private socket$?: WebSocketSubject<any>;
 
   // BehaviorSubject to handle server errors
   private errorSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
   error$: Observable<string> = this.errorSubject.asObservable();
 
-  constructor() {
-    this.socket$ = webSocket(environment.wsUrl);
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.initWebSocket();
+    }
+  }
 
-    this.socket$.subscribe(
-      (message) => {
-        if (message.error) {
-          this.errorSubject.next(message.error); // Emit error message
-        } else {
-          console.log('Server response:', message);
-          // Handle successful response as needed
+  private initWebSocket() {
+    try {
+      this.socket$ = webSocket(environment.wsUrl);
+
+      this.socket$.subscribe(
+        (message) => {
+          if (message.error) {
+            this.errorSubject.next(message.error); // Emit error message
+          } else {
+            console.log('Server response:', message);
+            // Handle successful response as needed
+          }
+        },
+        (error) => {
+          console.error('WebSocket error:', error);
+          this.errorSubject.next('WebSocket connection error'); // Emit connection error
         }
-      },
-      (error) => {
-        console.error('WebSocket error:', error);
-        this.errorSubject.next('WebSocket connection error'); // Emit connection error
-      }
-    );
+      );
+    } catch (error) {
+      console.error('Error initializing WebSocket:', error);
+      this.errorSubject.next('WebSocket initialization error');
+    }
   }
 
   send(message: any) {
-    this.socket$.next(message);
+    if (this.socket$) {
+      this.socket$.next(message);
+    } else {
+      console.error('WebSocket is not initialized.');
+    }
   }
 
   receive() {
-    return this.socket$.asObservable();
+    if (this.socket$) {
+      return this.socket$.asObservable();
+    } else {
+      console.error('WebSocket is not initialized.');
+      return EMPTY; // or handle differently
+    }
   }
 }
