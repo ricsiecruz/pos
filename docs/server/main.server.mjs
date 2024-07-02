df0650215aa5fe539bf595b1bf89c5257e73622b
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-07-02 10:52:02 +0800

    API_URL = 'https://pos-backend-kt9t.vercel.app/'; in expenses component

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-IFP4I33D.mjs";import"./chunk-5GKE4JNY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-7FK2OXID.mjs";import"./chunk-5GKE4JNY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
