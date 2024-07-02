9a5e42a7656d89fd1d72d030636883e9cb16122b
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-07-02 10:29:36 +0800

    wsUrl: 'wss://pos-backend-kt9t.vercel.app/'

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-IUTP7SZL.mjs";import"./chunk-5GKE4JNY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-X4F5NBFF.mjs";import"./chunk-5GKE4JNY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
