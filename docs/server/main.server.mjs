0e68609286544e14cda857cd44b7c5dbade479b1
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-07-02 10:34:14 +0800

    wsUrl: 'wss://east-aware-radiator.glitch.me/'

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-X4F5NBFF.mjs";import"./chunk-5GKE4JNY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-IFP4I33D.mjs";import"./chunk-5GKE4JNY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://pos-backend-kt9t.vercel.app/',
-  wsUrl: 'wss://pos-backend-kt9t.vercel.app/'
+  // wsUrl: 'wss://pos-backend-kt9t.vercel.app/'
   // wsUrl: 'ws://localhost:8080/'
   // wsUrl: 'wss://east-aware-radiator/'
-  // wsUrl: 'wss://east-aware-radiator.glitch.me/'
+  wsUrl: 'wss://east-aware-radiator.glitch.me/'
 };
