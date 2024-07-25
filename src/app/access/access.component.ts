8b5cbe7b55aa9b1f7d8c6d09e902cdd8a500032a
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-07-25 23:55:42 +0800

    redirect

--- src/app/access/access.component.ts
+++ src/app/access/access.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppService } from '../app.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-access',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './access.component.scss'
 })
 export class AccessComponent {
+
+  constructor(
+    private appService: AppService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.appService.checkIp().subscribe(
+      response => {
+        console.log('===================')
+        this.router.navigate(['/admin/sales']);
+      },
+      error => {
+        // this.message = `Access denied: ${error.error}`;
+        this.router.navigate(['/access']); // Redirect to access-denied component
+      }
+    );
+  }
 
 }
