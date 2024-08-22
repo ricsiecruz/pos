0f09b89d93e1296a2ef439c496f835f3c08262e4
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-08-23 00:12:36 +0800

    vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
 
   // prod
-  // apiUrl: 'https://pos-backend-kt9t.vercel.app/',
-  apiUrl: 'https://benevolent-brioche-d29205.netlify.app',
+  apiUrl: 'https://pos-backend-kt9t.vercel.app/',
+  // apiUrl: 'https://benevolent-brioche-d29205.netlify.app',
   wsUrl: 'wss://east-aware-radiator.glitch.me/'
 
   // local
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   // prod
-  // apiUrl: 'https://pos-backend-kt9t.vercel.app/',
-  apiUrl: 'https://benevolent-brioche-d29205.netlify.app',
+  apiUrl: 'https://pos-backend-kt9t.vercel.app/',
+  // apiUrl: 'https://benevolent-brioche-d29205.netlify.app',
   wsUrl: 'wss://east-aware-radiator.glitch.me/'
 
   // local
