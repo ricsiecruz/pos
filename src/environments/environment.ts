c4b480898c3c8addbb35b36f7168c8a107ce805f
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-09-02 20:15:57 +0800

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,9 @@ export const environment = {
   production: false,
   // prod
   apiUrl: 'https://pos-backend-kt9t.vercel.app/',
-  // apiUrl: 'https://benevolent-brioche-d29205.netlify.app',
   wsUrl: 'wss://east-aware-radiator.glitch.me/'
 
   // local
   // apiUrl: 'http://localhost:3000/',
-  // wsUrl: 'ws://localhost:8080/'
+  // wsUrl: 'ws://localhost:3000/'
 };
