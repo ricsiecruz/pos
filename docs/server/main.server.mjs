a269a4be2c5c084876f47090c746540ecbaa23e5
ricsie_kasim <ricsie.a@cvlinens.com>
2025-04-25 22:41:10 +0800

    deploy

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ECRA6JPD.mjs";import"./chunk-NGMPCEP2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-U2LBCUSS.mjs";import"./chunk-NGMPCEP2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    // apiUrl: 'https://pos-backend-kt9t.vercel.app/'
-    apiUrl: 'http://localhost:3000/'
+    apiUrl: 'https://pos-backend-kt9t.vercel.app/'
+    // apiUrl: 'http://localhost:3000/'
 };
